b143b2449ec8e41ef11cd1085d5330adde63a064
宇治松童磨 <xcourage.of.amaze@gmail.com>
2024-12-30 16:14:06 +0800

    Milestone 2 Revision 3: - Minor tweak: fix an error that causes the app to crash
    “里程碑”第二测试版第三修订版
    - 修复了一个导致app崩溃的问题。

--- app/src/main/java/prac/tanken/shigure/ui/subaci/ui/component/VoicesFlowRow.kt
+++ app/src/main/java/prac/tanken/shigure/ui/subaci/ui/component/VoicesFlowRow.kt
@@ -14,7 +14,7 @@ import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -45,7 +45,7 @@ fun VoicesFlowRow(
             }
         }
     } else {
-        var linesShown by rememberSaveable(voices) { mutableIntStateOf(initialMaxLines) }
+        var linesShown by remember(voices) { mutableIntStateOf(initialMaxLines) }
 
         val moreOrLessIndicator = @Composable { scope: ContextualFlowRowOverflowScope ->
             val remainingItems = totalItems - scope.shownItemCount
